8a0df044d01ea011aa0e6b806741eba3a737a648
Er-Deepanshu <erdeepanshubharadwaj@gmail.com>
2021-07-15 23:48:12 +0530

    First Commit

---
+++ Authorization-Microservice/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into users (id, password, user_name) values (1, 'admin', 'admin');
+insert into users (id, password, user_name) values (2, 'pallav', 'pallav');
+insert into users (id, password, user_name) values (3, 'deepanshu', 'deepanshu');
+insert into users (id, password, user_name) values (4, 'vivek', 'vivek');
+insert into users (id, password, user_name) values (5, 'deepika', 'deepika');
